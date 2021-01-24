ba270da9d8ed6b898b08e7c9aa1818dc2064b758
deanbunn <deanbunn@gmail.com>
2021-01-24 09:29:45 -0800

    20210124_0929hrs arrow functions and classes

--- W3JS/w3js_01.js
+++ W3JS/w3js_01.js
@@ -1,13 +1,57 @@
 
 
+//Testing Classes
+class Pet {
+    constructor(name,favorite_food) {
+	this.name = name;
+        this.food = favorite_food;
+    }
+
+    speak(p_msg) {
+	return this.name + ' says ' + p_msg;
+    }
+
+}
+
+let pups1 = new Pet("Apollo","Shredded Cheese");
+let kitty1 = new Pet("Jazzy","Steak");
 
 
+console.log(pups1.name);
+console.log(kitty1.food);
+console.log(pups1.speak('bark bark'));
 
+/*
+//Testing Arrow function
+sayHowdy = () => {
+   return 'Howdy';
+}
 
+console.log(sayHowdy());
+*/
 
+/*
+//Testing var vs let
+console.log(b20);
+console.log(b21);
+var b20 = 'Nova';
+let b21 = 'Super';
+//console.log(b20);
+//console.log(b21);
+*/
+
+/*
+//Testing Hoistering
+bx1 = 6;
+
+console.log(bx1);
+
+var bx1;
+*/
 
 
 /*
+//Testing booleans
 let b1 = false;
 
 if(b1 == true){
@@ -21,6 +65,7 @@ else{
 
 
 /*
+//Testing math functions
 console.log(Math.random());
 console.log(Math.floor(3.4));
 console.log(Math.floor(Math.random() * 10));
@@ -28,7 +73,8 @@ console.log(Math.floor(Math.random() * 100) + 1);
 */
 
 
-
+/*
+//Testing basic functions
 function milkDay(n1, n2) {
    return n1 * n2;
 }
@@ -47,11 +93,12 @@ var techie = { firstName: "Ken",
 
 
 //console.log(techie.displayName());
+*/
 
-
-var n3 = 10.4539;
 
 /*
+//Testing more math functions
+var n3 = 10.4539;
 console.log(n3.toExponential());
 console.log(n3.toExponential(3));
 console.log(n3.toFixed(0));
@@ -59,9 +106,10 @@ console.log(n3.toFixed(2));
 console.log(n3.toFixed(4));
 */
 
-var books = ["PowerShell", "NMAP", "Django"];
 
 /*
+//Testing arrays
+var books = ["PowerShell", "NMAP", "Django"];
 
 console.log(books);
 books.shift();
@@ -85,21 +133,22 @@ for(i=0; i < books.length; i++) {
 
 */
 
+
+/*
+//Testing Dates
+
 var d1 = new Date();
-
-//console.log(d1);
-//console.log(d1.toDateString());
-//console.log(d1.toISOString());
-//console.log(d1.getHours());
-//console.log(d1.getDay());
-//console.log(Date.now());
-
-
+console.log(d1);
+console.log(d1.toDateString());
+console.log(d1.toISOString());
+console.log(d1.getHours());
+console.log(d1.getDay());
+console.log(Date.now());
+*/
 
 
 
-
-
-
+console.log('\n\n');
+console.log('------- End of Testing --------');
 
 
