3f0e81e19cd04e321cfa6acb37260e3448381886
deanbunn <deanbunn@gmail.com>
2021-03-28 21:24:23 -0700

    20210328_2122hrs Pirple parsing HTTP methods

---
+++ PNMC/Henka/index.js
@@ -0,0 +1,36 @@
+/*
+* Primary file for the API
+*
+*/
+
+//Dependencies
+const http = require('http');
+const url = require('url');
+
+
+// The server should respond to all requests with a string
+const server = http.createServer(function(req,res){
+
+	// Get the URL and parse it
+	var parsedUrl = url.parse(req.url,true);	
+
+        // Get the path
+	var path = parsedUrl.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+	// Get the HTTP Method
+	var method = req.method.toLowerCase();
+
+	// Send the Response
+	res.end('Hello Dean\n');
+
+	// Log the request path
+	console.log('Request received on path: ' + trimmedPath + ' with method ' + method);
+	
+});
+
+
+// Start the server, and have it listen on port 3000
+server.listen(3000,function(){
+	console.log('The server is listening on port 3000 now');
+});
